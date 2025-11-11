673ad75637439ebbcd65b402e70ca99b34ab4ced
Patrick-usama-16002527 <patrickossama@yahoo.com>
2025-11-11 09:36:44 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -6,17 +6,26 @@
 using namespace std;
 //#write your code here
 
+ class course {
 
+private :
+string coursecode ;
+string coursename ;
+int maxstudents ;
+Student* students ;
+int currentstudents ;
 
+public :
 
+addStudent (const Student& s );
+displaycourseinfo();
+course();
+course();
+setcourse();
+getcourse();
 
+  
 
-
-
-
-
-
-
-
+}
 
 #endif
